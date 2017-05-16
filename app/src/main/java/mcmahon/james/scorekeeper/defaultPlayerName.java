6ee657fff5777e8bce3922caaaeed881f512f505
james <moohan@users.noreply.github.com>
2017-05-16 17:09:24 +0100

    Initial commit

---
+++ app/src/main/java/mcmahon/james/scorekeeper/defaultPlayerName.java
@@ -0,0 +1,18 @@
+package mcmahon.james.scorekeeper;
+
+/**
+ * Created by james on 16/05/17.
+ */
+
+public class defaultPlayerName {
+    private final String value;
+
+    public defaultPlayerName(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+
+    }
+}
